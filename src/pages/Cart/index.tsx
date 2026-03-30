20a75573af28982c1427799beeeeb424fb288d36
Ilya Olovyannikov <excaliburgyro@gmail.com>
2026-03-30 11:10:00 +0300

    feat(features): decompose features

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,13 +1,13 @@
 import { createLazyRouteView } from '@argon-router/react';
 
 import { routes } from '@/shared/router';
-import { PageLoader } from '@/shared/ui';
+import { PageLoader } from '@/shared/ui/PageLoader';
 
 import { RootLayout } from '@/widgets/root-layout';
 
 export const CartPageLazyScreen = createLazyRouteView({
     route: routes.cart,
-    view: () => import('./ui/page.tsx'),
     fallback: PageLoader,
-    layout: RootLayout,
+    view: () => import('./ui/page.tsx'),
+    layout: ({ children }) => <RootLayout title='DogFood | Корзина'>{children}</RootLayout>,
 });
